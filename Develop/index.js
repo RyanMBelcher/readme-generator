a18eabccfeb21783eccff1492f302f6b4e5c4ed1
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-02 16:15:05 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,58 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username'
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: 'What is your project name?',
+            name: 'project'
+        },
+        {
+            type: 'input',
+            message: 'Please write a short description.',
+            name: 'description'
+        },
+        {
+            type: 'list',
+            message: 'What kinds of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3.0', 'None'],
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'What command should be run to run test?',
+            default: 'npm test',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about using this repo?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about contributing to the repo?',
+            name: 'contributing'
+        }
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
