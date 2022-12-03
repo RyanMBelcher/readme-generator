b35adf998caef0ab9bc74831c873cf6dce42ba68
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-03 15:58:30 -0500

    added basic text to generatemarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
 
+  # ${data.project}
+
+  ![License](https://img.shields.io/badge/License-${data.license}-blue);
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation]
+  - [Usage]
+  - [License]
+  - [Credit]
+  - [Questions]
+  - [Tests]
+
+  ## Installation 
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Credit
+
+
+  ## Questions 
+  ${data.contributing}
+
+  ## Tests
+  ${data.test}
 `;
 }
 
